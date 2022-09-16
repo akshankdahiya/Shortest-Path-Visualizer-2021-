ec788f618c4bb3eb44f7b5871fb82c487673e983
Akshank Dahiya <97908366+akshankdahiya@users.noreply.github.com>
2022-09-16 16:17:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import PathfindingVisualizer from './PathfindingVisualiser/PathfindingVisualiser';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
